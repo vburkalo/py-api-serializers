3e2cff7d10fe859016db63840011e464ab561d0b
vital <vitaliyburkalo81@gmail.com>
2024-05-12 11:38:20 +0300

    implemented endpoints for the app

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,24 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import (
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet
+)
+
+app_name = "cinema"
+
+router = routers.DefaultRouter()
+
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("cinemaHalls", CinemaHallViewSet)
+router.register("movies", MovieViewSet)
+router.register("movieSessions", MovieSessionViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
